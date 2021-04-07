6a547b1c33449a5a19ce5c8c2fca8e52808c71f2
nardos111 <nardos2alemu@gmail.com>
2021-04-07 18:23:33 +0300

    Add selenium automation tool

---
+++ comment.py
@@ -0,0 +1,6 @@
+import selenium
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
