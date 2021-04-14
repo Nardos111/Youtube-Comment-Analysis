205b61c31aa504aaf60d614ecf5bc35e703d819f
nardos111 <nardos2alemu@gmail.com>
2021-04-15 02:36:47 +0300

    Calcuate polarity score

--- SA_with_nltk.py
+++ SA_with_nltk.py
@@ -1,10 +1,23 @@
 
 
 import csv
-# import nltk
+import nltk
+import text2emotion as te
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 
 comments = []
 with open('results.csv', 'r', encoding="utf-16") as file:
     reader = csv.reader(file)
     for row in reader:
         comments.append(row[1])
+combined_comments = ' '.join(comments)
+word: list[str] = nltk.word_tokenize(combined_comments)
+stopwords = nltk.corpus.stopwords.words("english")
+words = [w for w in word if w.lower() not in stopwords and w.isalpha()
+         and len(w) > 2]
+fd = nltk.FreqDist(words)  # frequent words used
+sa = SentimentIntensityAnalyzer()
+print(sa.polarity_scores(combined_comments))
+print(fd.most_common(10))
+print(te.get_emotion(combined_comments))
